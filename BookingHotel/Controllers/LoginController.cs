13417816b9c89f221acb9a7cd67b9fa9f202ad0d
Admin <Admin@ADMIN-PC>
2023-10-05 16:14:11 +0700

    Update 05/10/2023

---
+++ BookingHotel/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHotel.Controllers
+{
+    [Route("admin/login")]
+    public class LoginController : Controller
+    {
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
