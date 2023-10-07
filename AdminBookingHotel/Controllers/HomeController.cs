e797307383bcb01230eb71d794f60991dee4e40e
Admin <Admin@ADMIN-PC>
2023-10-07 14:58:12 +0700

    update 07/10/2023

---
+++ AdminBookingHotel/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using AdminBookingHotel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AdminBookingHotel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
